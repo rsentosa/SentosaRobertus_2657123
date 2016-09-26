7622b262fbbc056ef77953d3e6fb927672618c85
rcc <rcc>
2016-09-26 09:51:33 -0700

    Signed-off-by: rcc <rcc>

---
+++ Lab/TruthTable/main.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.cpp
+ * Author: Robertus Sentosa
+ * Created on September 26, 2016, 9:37 AM
+ * Purpose:  Display 13 columns of the truth table
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    bool x,y;
+   
+    //output the heading of the truth table
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^Y X^Y^X !(X&&Y) !X||!Y !(X||Y) !X&&!Y\n";
+   
+    //Input values for first row
+    x=true;
+    y=true;
+    
+    //display the first row of the truth table
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<" ";
+    cout<<endl;
+    
+    //Input values for second row of the truth table
+    y=false;
+    
+    //Display the second row of the truth table
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<" ";
+    cout<<endl;
+    //Exit
+    return 0;
+}
+
